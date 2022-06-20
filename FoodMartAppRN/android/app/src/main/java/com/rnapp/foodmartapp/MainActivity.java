3b3e728e633c23299ba4191592f1cd45ec8413fd
moohat <refixzcorp@gmail.com>
2022-06-20 07:46:09 -0700

    setup atomic design structure

--- FoodMartAppRN/android/app/src/main/java/com/rnapp/foodmartapp/MainActivity.java
+++ FoodMartAppRN/android/app/src/main/java/com/rnapp/foodmartapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.docapp;
+package com.rnapp.foodmartapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "DocApp";
+    return "rnapp.foodmartapp";
   }
 }
